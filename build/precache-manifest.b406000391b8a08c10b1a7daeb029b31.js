3e741a533468804deecb4986b9e5326c1491a3d5
Bayzid Alim <67591935+bayzidalim@users.noreply.github.com>
2020-10-02 18:29:18 +0600

    Created Build

---
+++ build/precache-manifest.b406000391b8a08c10b1a7daeb029b31.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eda711789df675a7622ffb2cdb1a9111",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5fb9775b9f9f54da8c99",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "0612f9951ae486f143ae",
+    "url": "/static/css/main.76e27cc9.chunk.css"
+  },
+  {
+    "revision": "5fb9775b9f9f54da8c99",
+    "url": "/static/js/2.d64d279b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.d64d279b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0612f9951ae486f143ae",
+    "url": "/static/js/main.a84fa866.chunk.js"
+  },
+  {
+    "revision": "8d05a02df7e1830b2b5e",
+    "url": "/static/js/runtime-main.0bdfd285.js"
+  }
+]);
